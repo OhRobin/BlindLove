59824635bd746273ebb8ecdc943c0fcf4064402f
changeonPark <qkrcksrjs@gmail.com>
2023-09-02 17:53:10 +0900

    style: set google font with tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,8 +2,14 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      display: ["Raleway"],
+      heading: ["Raleway"],
+      "heading-4": ["Poppins"],
+      body: ["Poppins"],
+      caption: ["Poppins"],
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
