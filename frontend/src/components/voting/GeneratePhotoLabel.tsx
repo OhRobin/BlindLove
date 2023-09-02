2323262d79906f64ed1b8ff7d8f81ae8eeeafe2a
hankn01 <kangminpersonal@naver.com>
2023-09-02 21:21:29 +0900

    Home screen Generate button modified

--- frontend/src/components/voting/GeneratePhotoLabel.tsx
+++ frontend/src/components/voting/GeneratePhotoLabel.tsx
@@ -1,7 +1,7 @@
 function GeneratePhotoLabel() {
     return(
-      <div className="rounded-full bg-white w-40 h-14 mx-auto">
-        <h3>+ Generate</h3>
+      <div className="rounded-full bg-white w-40 h-14 mx-auto py-4">
+        + Generate
       </div>  
     );
 }
