53000db973f62fcf9a60814e98436d7d824928d8
hankn01 <kangminpersonal@naver.com>
2023-09-02 20:54:53 +0900

    Voting Screen Title Bar Implemented

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { RecoilRoot } from "recoil";
 import { Routes, Route, Outlet, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Receivevotes from "./pages/Receivevotes";
+import VotingBefore from "./pages/VotingBefore";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
             <Route path="/" element={<AppLayout />}>
               <Route index element={<Home />} />
               <Route path="/recvotes" element={<Receivevotes />} />
+              <Route path="/voting" element={<VotingBefore />} />
             </Route>
           </Routes>
         </BrowserRouter>
